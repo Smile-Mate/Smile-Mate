c9f999392cfb9e2a415fa355d99eac2fdf008026
lunch0 <lunchboxno@gmail.com>
2025-02-20 19:22:10 +0900

    feat: 인풋 비교

--- containers/LoginContainer.tsx
+++ containers/LoginContainer.tsx
@@ -18,11 +18,9 @@ export default function LoginContainer() {
             title="식별 번호를 입력해주세요"
             description={`식별 번호는 사전에 안내드린\n문자 메시지에서 확인할 수 있어요.`}
           />
-          <input
-            type="text"
-            className="mt-20
-           p-3 border-[0.4px] border-neutral-500 rounded-lg"
-          />
+          <input type="text" className="mt-20 p-3 border-[0.4px] border-neutral-500 bg-[#F4F4F4] rounded-lg" />
+          <input type="text" className="mt-5 p-3 border-[0.4px] border-neutral-500 rounded-lg" />
+          <input type="text" className="mt-5 p-3 border-neutral-500 rounded-lg" />
         </PaddingBlock>
       </Container>
       <BottomPortal>

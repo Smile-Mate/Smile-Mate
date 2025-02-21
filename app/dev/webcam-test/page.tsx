ff9f69b9c521bff6406aea28e35be1a65514fffa
lunch0 <lunchboxno@gmail.com>
2025-02-21 11:59:29 +0900

    feat: 웹캠 테스트 페이지 추가

---
+++ app/dev/webcam-test/page.tsx
@@ -0,0 +1,9 @@
+import DevWebcamTestContainer from '@/containers/dev/DevWebcamTestContainer';
+
+export default async function DevWebcamTestPage() {
+  return (
+    <>
+      <DevWebcamTestContainer />
+    </>
+  );
+}
---
+++ containers/dev/DevWebcamTestContainer.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import BottomPortal from '@/components/BottomPortal';
+import Container from '@/components/Container';
+
+export default function DevWebcamTestContainer() {
+  return (
+    <>
+      <Container>
+        <div></div>
+      </Container>
+      <BottomPortal>
+        <div></div>
+      </BottomPortal>
+    </>
+  );
+}

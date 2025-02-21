a6a53143cd332883cdb21a9fffdec03bb0ae623b
lunch0 <lunchboxno@gmail.com>
2025-02-22 00:18:04 +0900

    feat: 미소짓기

--- components/dev/WebcamComponent.tsx
+++ components/dev/WebcamComponent.tsx
@@ -15,7 +15,7 @@ const defaultAvatarUrl =
 const options: FaceLandmarkerOptions = {
   baseOptions: {
     modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
-    delegate: 'CPU',
+    delegate: 'GPU',
   },
   numFaces: 1,
   runningMode: 'VIDEO',
@@ -36,6 +36,7 @@ export default function WebcamComponent({ setIsSuccess }: { setIsSuccess: (succe
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const [happyCount, setHappyCount] = useState(0);
   const animationFrameIdRef = useRef<number | null>(null); // animationFrame ID 저장
+  const [detectCount, setDetectCount] = useState(0);
 
   const setup = async () => {
     const filesetResolver = await FilesetResolver.forVisionTasks(
@@ -48,7 +49,7 @@ export default function WebcamComponent({ setIsSuccess }: { setIsSuccess: (succe
 
     navigator.mediaDevices
       .getUserMedia({
-        video: true,
+        video: { width: 1280, height: 720 },
         audio: false,
       })
       .then(function (stream) {
@@ -82,6 +83,8 @@ export default function WebcamComponent({ setIsSuccess }: { setIsSuccess: (succe
     await loadFaceApiModels();
 
     const detect = async () => {
+      setDetectCount(prev => prev + 1);
+
       const detections = await faceapi
         .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
         .withFaceExpressions();
@@ -96,7 +99,7 @@ export default function WebcamComponent({ setIsSuccess }: { setIsSuccess: (succe
         if (typeof score === 'number') {
           setExpression(maxExpression);
           setExpressionScore(score);
-          if (maxExpression === 'happy' && score > 0.75) {
+          if (maxExpression === 'happy' && score > 0.5) {
             setHappyCount(prev => {
               console.log('happyCount: ', prev + 1);
               return prev + 1;
@@ -155,16 +158,14 @@ export default function WebcamComponent({ setIsSuccess }: { setIsSuccess: (succe
 
   return (
     <div className="flex flex-col items-center justify-center">
+      <div>detectCount:{detectCount}</div>
+      <div>happyCount:{happyCount}</div>
       <video ref={videoRef} id="video" style={{ width: '1px', height: '1px', opacity: 0 }} autoPlay muted />
 
       <Canvas style={{ height: 240, width: '100%' }} camera={{ fov: 60, position: [0, 1, 5] }}>
-        {/* @ts-ignore */}
         <ambientLight intensity={1.5} />
-        {/* @ts-ignore */}
         <pointLight position={[10, 10, 10]} color={new Color(1, 1, 0)} intensity={1.75} />
-        {/* @ts-ignore */}
         <pointLight position={[-10, 0, 10]} color={new Color(1, 0, 0)} intensity={1.75} />
-        {/* @ts-ignore */}
         <pointLight position={[0, 0, 10]} intensity={1.75} />
         <Avatar url={defaultAvatarUrl} blendshapes={blendshapes} rotation={rotation} />
       </Canvas>

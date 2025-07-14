f01334d7421719daf0a0bcaae3627634c746cd0f
boonddiga <boonddiga00@naver.com>
2025-07-15 01:51:35 +0900

    Fix: faceapi fs error

--- utils/faceApiUtil.ts
+++ utils/faceApiUtil.ts
@@ -1,6 +1,6 @@
-import * as faceapi from "face-api.js";
+import * as faceapi from 'face-api.js';
 
-export const loadFaceApiModels = async () => {
+/* export const loadFaceApiModels = async () => {
   const MODEL_URL = "/models";
 
   await Promise.all([
@@ -9,5 +9,23 @@ export const loadFaceApiModels = async () => {
     faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
     faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
     faceapi.nets.ageGenderNet.loadFromUri(MODEL_URL),
+  ]);
+}; */
+
+declare global {
+  interface Window {
+    faceapi: any; // 필요시 more specific하게 지정 가능
+  }
+}
+
+export const loadFaceApiModels = async () => {
+  const MODEL_URL = '/models';
+
+  await Promise.all([
+    window.faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+    window.faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+    window.faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+    window.faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+    window.faceapi.nets.ageGenderNet.loadFromUri(MODEL_URL),
   ]);
 };
